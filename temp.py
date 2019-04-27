6d1fafacd6538470661de52e920a6fc78dfd612a
Javier <jajimenez8@hotmail.com>
2019-04-27 10:42:09 -0500

    Tipos de Datos

--- temp.py
+++ temp.py
@@ -1,9 +1,45 @@
 # -*- coding: utf-8 -*-
 """
-Editor de Spyder
-
-Este es un archivo temporal.
+Comentario
+de parrafo
 """
 
-a = 3
-A = 7
+# Hola Mundo
+print("Hola Mundo")
+
+# Variables
+x = 5  # Int
+y = "Texto"  # String
+z = 4.5  # Float
+a = True  # Bool
+b = False  # Bool
+c = 4+5j  # Complejo
+
+print(x, y, z, a, b, c)
+
+# Conocer el tipo de dato
+print(type(b))
+
+# Enteros
+x = int(2)
+print(x)
+x = int("3")
+print(x)
+
+# Float
+x = float(2)
+print(x)
+x = float(2.5)
+print(x)
+x = float("2")
+print(x)
+x = float("2.5")
+print(x)
+
+# string
+x = str(2)
+print(x)
+y = str("3")
+print(x)
+z = str("2")
+print(x)
